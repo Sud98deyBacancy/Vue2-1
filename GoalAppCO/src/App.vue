14eb11eab8a7fd0127f1daafc0a332e0c703bc7c
Sud98-dey <75294216+Sud98-dey@users.noreply.github.com>
2021-03-10 16:04:42 +0530

    commit

---
+++ GoalAppCO/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <h2>My Course Goal</h2>
+  <!-- Task 1: Output your main course goal with help of the composition API -->
+  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+  <h3 v-if="visible">COURSE GOAL : {{ Goal }}</h3>
+  <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+  <button @click="ToggleGoal">{{ text}} Goal</button>
+  <!-- Task 3: Manage data in three ways -->
+  <!-- => Separate refs -->
+  <!-- => Ref Object -->
+  <!-- => Reactive Object -->
+  <!-- Task 4: Also solve the assignment with the Options API -->
+</template>
+<script>
+//import {ref} from 'vue';
+//import {reactive} from 'vue';
+export default {
+/* Toggle goal using Composition
+   setup(){
+   //  const Goal=ref('Learn VueJS');//Seperate Ref
+  //   const Display=ref({visible:true,text:'Toogle'}); // Ref Object
+    const Goal={ title:'Learn VueJS',visible:true,text:'Toogle'}
+     const Display = reactive(Goal);// Reactive Object
+//  function Toggle1(){ Display.value.visible=!Display.value.visible;  }
+   function Toggle2() { Display.visible = !Display.visible;}
+return { Display,Toggle2};
+   }  
+  Toggle goal using Options API **/
+data(){ 
+  return { Goal:'Learn VueJS',visible:true,text:'Toggle' };
+},
+methods:{
+  ToggleGoal(){ this.visible=!this.visible;}
+}
+}
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 3rem;
+  text-align: center;
+}
+</style>
---
+++ USerApp_CompositionAPI/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <section class="container">
+    <!--<h2>{{ userName }} <span> {{age}}</span></h2>-->
+        <h2>{{ user.name }} <span> {{user.age}}</span></h2>
+        <button @click="setData"> Change </button> </section>
+        <div class="container">
+        <input type="text" placeholder="Enter First Name" v-model="Fname"/><br>
+        <input type="text" placeholder="Enter Last Name" v-model="Lname"/>
+        </div>
+  
+</template>
+
+<script>
+import { reactive,ref,computed } from 'vue';
+export default {
+ // data() {return {userName: 'Maximilian' };  },
+setup(){
+  // Passing value in ref const name=ref('Michael'); const age=ref(37);
+  // Passing Object in ref const user= ref({name:'Mike',age:39});
+  //  setTimeout(function(){  /*name.value = 'Gabriel',age.value=34*/ user.value.name='Luci',user.value.age=37 user.name='Luci',user.age=37},4000); 
+  
+  const Fname = ref(''); const Lname = ref('');
+  const user= reactive({name:'Mike',age:39});
+
+   function setNewData(){ user.name='Luce'; user.age+=10;}
+  // function setFname(event){ Fname.value = event.target.value; }
+  // function setLname(event){ Lname.value = event.target.value; }
+   let uName=computed(function(){return Fname.value + ' ' + Lname.value });  
+   user.name=uName;
+return { user:user,setData:setNewData,Fname,Lname};
+}
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body,input {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
+}
+</style>
